acd3f930dd90acabcac88425e2884e8fead60d6f
Priyansi <priyanhiman@gmail.com>
2020-04-08 16:36:02 +0530

    Made pattern1 readable

--- pattern1.py
+++ pattern1.py
@@ -6,13 +6,20 @@
 
 import sys
 
+space = ' '
+asterisk = '*'
+
 
 def spaces(x: int) -> str:
-    return ''.rjust(x, ' ')
+    return ''.rjust(x, space)
+
+
+def asterisks(x: int) -> str:
+    return ''.rjust(x, asterisk)
 
 
 def pattern(num: int) -> str:
-    return ''.join(spaces(num-i-1)+''.rjust(i*2+1, '*')+'\n' for i in range(num))
+    return ''.join(spaces(num-(i-1))+asterisks((2*i)+1)+'\n' for i in range(num))
 
 
 print(pattern(int(sys.argv[1])))
