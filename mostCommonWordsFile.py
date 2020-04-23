5bda4ff6f563212b8a6d3e8f3da06d3fc722f2c6
priyansi <priyanhiman@gmail.com>
2020-04-24 03:07:09 +0530

    meh

--- mostCommonWordsFile.py
+++ mostCommonWordsFile.py
@@ -1,15 +1,21 @@
+import sys
 import re
 import collections
 
-NO_OF_MOST_COMMONS=10
+NO_OF_MOST_COMMONS = 10
+EXCLUDE_STOPWORDS = 'y'
 
-def mostCommonWords():
-    with open('WarAndPeace.txt','r') as f:
+
+def mostCommonWords(EXCLUDE_STOPWORDS='y'):
+    with open('WarAndPeace.txt', 'r') as f:
         text = f.read().lower()
-
     words = re.findall(r'\w+', text)
+    if EXCLUDE_STOPWORDS.lower() == 'n':
+        with open('stopwords.txt', 'r') as f:
+            stopwords = f.read().split()
+        words = [word for word in words if word not in stopwords]
     wordCount = collections.Counter(words)
-    return ''.join('{} : {}\n'.format(word,count) for word, count in wordCount.most_common(NO_OF_MOST_COMMONS))
+    return ''.join('{} : {}\n'.format(word, count) for word, count in wordCount.most_common(NO_OF_MOST_COMMONS))
 
 
-print(mostCommonWords())
+print(mostCommonWords(EXCLUDE_STOPWORDS))
