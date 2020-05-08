be2b3bba628aff7d0b1f30cd3bb53fb171eba643
priyansi <priyanhiman@gmail.com>
2020-05-08 17:37:39 +0530

    modify latex

--- latexPolyReverse.py
+++ latexPolyReverse.py
@@ -2,15 +2,21 @@
 # OUTPUT - $-7 +x -3x^2$
 import re
 
+PLUS = '+'
+MINUS = '-'
+SPACE = ' '
+DOLLAR = '$'
+EMPTY_STRING = ''
+
 
 def convert_to_common_pattern(poly):
-    return poly.replace('+', ' +').replace('-', ' -')
+    return poly.replace(PLUS, SPACE+PLUS).replace(MINUS, SPACE+MINUS)
 
 
 def reverse_poly(poly):
     common_pattern = convert_to_common_pattern(
-        poly.strip('$').replace(' ', ''))
-    return '$'+' '.join(common_pattern.split(' ')[::-1]).strip('+ ')+'$'
+        poly.strip(DOLLAR).replace(SPACE, EMPTY_STRING))
+    return DOLLAR + SPACE.join(common_pattern.split(SPACE)[::-1]).strip(PLUS+SPACE) + DOLLAR
 
 
 print(reverse_poly('$-3  x  ^    2 + x - 7   $'))
